5a2619029ef00f8c390b4f1733ff08e1e3a6e853
GiovannaBatistaG <giovanna.batis123@gmail.com>
2025-06-03 22:16:13 -0300

    Update FooterComponents.vue

--- src/components/FooterComponents.vue
+++ src/components/FooterComponents.vue
@@ -1,23 +1,9 @@
 <template>
   <footer class="bg-gray-300 text-gray-900 py-8 mt-12">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-      <p class="text-sm">&copy; {{ new Date().getFullYear() }} Sua Loja Inc. Todos os direitos reservados.</p>
-      <div class="mt-4 flex justify-center space-x-6">
-        <a href="#" class="text-gray-400 hover:text-white transition">
-          <i class="fab fa-facebook-f"></i>
-          <span class="sr-only">Facebook</span>
-        </a>
-        <a href="#" class="text-gray-400 hover:text-white transition">
-          <i class="fab fa-instagram"></i>
-          <span class="sr-only">Instagram</span>
-        </a>
-        <a href="#" class="text-gray-400 hover:text-white transition">
-          <i class="fab fa-twitter"></i>
-          <span class="sr-only">Twitter</span>
-        </a>
-      </div>
+      <p class="text-sm">&copy; {{ new Date().getFullYear() }}  Todos os direitos reservados.</p>
       <p class="mt-4 text-xs text-gray-500">
-        Desenvolvido por Giovanna e MArcelo   </p>
+        Desenvolvido por Marcelo e Giovanna</p>
     </div>
   </footer>
 </template>
